3b80f935002019299b88b599ea9ecad734d0f625
Tarasdeveloper <tomahavki1@gmail.com>
2023-04-27 12:22:50 +0300

    Updated

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(item => {
+    return `<li class="gallery__item">
+                <a class="gallery__link" href="${item.original}">
+                    <img
+                        class="gallery__image"
+                        src="${item.preview}" 
+                        alt="${item.description}"
+                    >
+                </a>
+            </li>`;
+  })
+  .join('');
+galleryList.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox('.gallery .gallery__link', {
+  //   captions: true,
+  //   captionsData: 'alt',
+  //   captionPosition: 'bottom',
+  //   captionDelay: 250,
+});
